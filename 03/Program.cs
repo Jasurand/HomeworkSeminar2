36eeb6aad728996aad9d15330658f3aa29353e16
Jasand <smileonyourface@yandex.com>
2023-11-30 23:02:58 +0500

    solved third task

---
+++ 03/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3
+// Напишите программу, которая принимает на вход целое число из отрезка [10, 99]
+// и показывает наибольшую цифру числа.
+
+Console.Clear();
+Console.Write("Введите, пожалуйста, любое число между 10 и 99: ");
+int number = int.Parse(Console.ReadLine()!);
+if (number < 10 || number > 99)
+    Console.WriteLine("Вы ввели неверное число!");
+else if (number / 10 == number % 10)
+    Console.WriteLine($"Обе цифры в этом числе равны ({number / 10})");
+else if ((number / 10) > (number % 10))
+    Console.WriteLine($"{number / 10} > {number % 10}");
+else if ((number / 10 < number % 10))
+    Console.WriteLine($"{number / 10} < {number % 10}");
